c9ca6781d36b9bd836a127d100dcbe2e178fa6f3
root <root@ip-172-31-20-23.us-west-2.compute.internal>
2015-02-19 20:01:18 +0000

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+  <head>
+    <title> PHP Test </title>
+  </head>
+  <body>
+    <?php echo '<p> The current time is:</p>'; ?>
+    <?php echo date("D M d, Y G:i a"); ?>
+    <?php echo '<p>....and Bob is cool</p>'; ?>
+  </body>
+<html>
